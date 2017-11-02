5c63f17c83222fdd84abbcb26dd265d4d6de91af
sergei <sergei@Mac-mini-sergei.local>
2017-11-03 02:55:11 +0300

    code refactoring

--- image_resize.py
+++ image_resize.py
@@ -5,39 +5,16 @@ import os.path
 from PIL import Image
 
 
-class ArgumentStorage:
+def get_arguments():
 
-    def __init__(self):
-
-        parser = argparse.ArgumentParser(description='image resizer')
-        parser.add_argument('image', help='source image path')
-        parser.add_argument('--width', help='result image width', type=int)
-        parser.add_argument('--height', help='result image height', type=int)
-        parser.add_argument('--scale', help='result image scale', type=float)
-        parser.add_argument('--output', help='result image path')
-        arguments = parser.parse_args()
-
-        self.image = arguments.image
-        self.width = arguments.width
-        self.height = arguments.height
-        self.scale = arguments.scale
-        self.output = arguments.output
-
-    @property
-    def has_width(self):
-        return self.width is not None
-
-    @property
-    def has_height(self):
-        return self.height is not None
-
-    @property
-    def has_scale(self):
-        return self.scale is not None
-
-    @property
-    def has_output(self):
-        return self.output is not None
+    parser = argparse.ArgumentParser(description='image resizer')
+    parser.add_argument('image', help='source image path')
+    parser.add_argument('--width', help='result image width', type=int)
+    parser.add_argument('--height', help='result image height', type=int)
+    parser.add_argument('--scale', help='result image scale', type=float)
+    parser.add_argument('--output', help='result image path')
+    args = vars(parser.parse_args())
+    return {key: value for key, value in args.items() if value is not None}
 
 
 class SizeCalculator:
@@ -54,82 +31,70 @@ class SizeCalculator:
     def size(self):
         return int(self.width), int(self.height)
 
-    def calculate(self, width=None, height=None, scale=None):
+    def _by_scale(self, scale):
+        self.width *= scale
+        self.height *= scale
 
-        if width is None and height is None and scale is not None:
-            self.width *= scale
-            self.height *= scale
+    def _by_width(self, width):
+        self.width = width
+        self.height = width / self.proportion
 
-        if width is not None and height is None and scale is None:
-            self.width = width
-            self.height = width / self.proportion
+    def _by_height(self, height):
+        self.height = height
+        self.width = height * self.proportion
 
-        if width is None and height is not None and scale is None:
-            self.height = height
-            self.width = height * self.proportion
+    def _by_width_height(self, width, height):
+        proportion = width / height
+        self.proportion_changed = proportion != self.proportion
+        self.width = width
+        self.height = height
 
-        if width is not None and height is not None and scale is None:
+    def calculate(self, arguments):
 
-            if width == self.width and height == self.height:
-                return
+        if {'scale'} < set(arguments):
+            self._by_scale(arguments['scale'])
 
-            proportion = width / height
-            self.proportion_changed = proportion != self.proportion
-            self.width = width
-            self.height = height
+        if {'width', 'height'} < set(arguments):
+            self._by_width_height(arguments['width'], arguments['height'])
+            return
 
+        if {'width'} < set(arguments):
+            self._by_width(arguments['width'])
 
-def calculate_new_size(storage, current_size):
-
-    calc = SizeCalculator(current_size)
-
-    if storage.has_scale:
-        calc.calculate(scale=storage.scale)
-
-    elif not storage.has_width:
-        calc.calculate(height=storage.height)
-
-    elif not storage.has_height:
-        calc.calculate(width=storage.width)
-
-    else:
-        calc.calculate(width=storage.width, height=storage.height)
-
-    return calc
+        if {'height'} < set(arguments):
+            self._by_height(arguments['height'])
 
 
 if __name__ == '__main__':
 
-    storage = ArgumentStorage()
+    arguments = get_arguments()
 
-    if all((
-        not storage.has_width,
-        not storage.has_height,
-        not storage.has_scale
-    )):
+    if not({'scale', 'width', 'height'} & set(arguments)):
 
         print('No one resize option has set')
         print('Run script with [--width, --height | --scale] options')
         sys.exit()
 
-    if (storage.has_width or storage.has_height) and storage.has_scale:
+    if ({'scale', 'width'} < set(arguments) or
+            {'scale', 'height'} < set(arguments)):
 
         print('Options conflict:')
         print('\t--width or --height are not compatible with --scale')
         sys.exit()
 
-    image = Image.open(storage.image)
-    calculator = calculate_new_size(storage, image.size)
+    image = Image.open(arguments['image'])
+    calculator = SizeCalculator(image.size)
+    calculator.calculate(arguments)
 
     if calculator.proportion_changed:
         print('WARNING: Image proportion has changed')
 
     result_image = image.resize(calculator.size)
 
-    if storage.has_output:
-        result_image.save(storage.output)
+    if {'output'} < set(arguments):
+        result_image.save(arguments['output'])
 
     else:
-        filename, file_ext = os.path.splitext(storage.image)
+        filename, file_ext = os.path.splitext(arguments['image'])
         image_filename = '{}__{}{}'.format(filename, calculator, file_ext)
         result_image.save(image_filename)
