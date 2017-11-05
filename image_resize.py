49c590fdb6a69156e4d52167b5ddbc4fe71c81d5
sergei <sergei@Mac-mini-sergei.local>
2017-11-06 01:37:40 +0300

    recommended changes accepted

--- image_resize.py
+++ image_resize.py
@@ -5,24 +5,13 @@ import os.path
 from PIL import Image
 
 
-def get_arguments():
-
-    parser = argparse.ArgumentParser(description='image resizer')
-    parser.add_argument('image', help='source image path')
-    parser.add_argument('--width', help='result image width', type=int)
-    parser.add_argument('--height', help='result image height', type=int)
-    parser.add_argument('--scale', help='result image scale', type=float)
-    parser.add_argument('--output', help='result image path')
-    args = vars(parser.parse_args())
-    return {key: value for key, value in args.items() if value is not None}
-
-
 class SizeCalculator:
 
     def __init__(self, size):
-        self.width, self.height = size
-        self.proportion = self.width / self.height
-        self.proportion_changed = False
+        self.initial_width, self.initial_height = size
+        self.initial_proportion = self.initial_width / self.initial_height
+        self.width, self.height = self.initial_width, self.initial_height
+        self.proportion = self.initial_proportion
 
     def __str__(self):
         return '{}x{}'.format(int(self.width), int(self.height))
@@ -30,6 +19,10 @@ class SizeCalculator:
     @property
     def size(self):
         return int(self.width), int(self.height)
+
+    @property
+    def proportion_changed(self):
+        return self.proportion != self.initial_proportion
 
     def _by_scale(self, scale):
         self.width *= scale
@@ -44,57 +37,69 @@ class SizeCalculator:
         self.width = height * self.proportion
 
     def _by_width_height(self, width, height):
-        proportion = width / height
-        self.proportion_changed = proportion != self.proportion
-        self.width = width
-        self.height = height
+        self.width, self.height = width, height
+        self.proportion = self.width / self.height
 
-    def calculate(self, arguments):
-
-        if {'scale'} < set(arguments):
-            self._by_scale(arguments['scale'])
-
-        if {'width', 'height'} < set(arguments):
-            self._by_width_height(arguments['width'], arguments['height'])
+    def calculate(self, scale, width, height):
+        if scale:
+            self._by_scale(scale)
             return
 
-        if {'width'} < set(arguments):
-            self._by_width(arguments['width'])
+        if width and height:
+            self._by_width_height(width, height)
+            return
 
-        if {'height'} < set(arguments):
-            self._by_height(arguments['height'])
+        if width:
+            self._by_width(width)
+
+        if height:
+            self._by_height(height)
+
+
+def resize_image(imagepath, scale, width, height, output):
+
+    image = Image.open(imagepath)
+
+    calculator = SizeCalculator(image.size)
+    calculator.calculate(scale, width, height)
+
+    result_image = image.resize(calculator.size, Image.ANTIALIAS)
+
+    image_filename = output
+    if not output:
+        filename, file_ext = os.path.splitext(imagepath)
+        image_filename = '{}__{}{}'.format(filename, calculator, file_ext)
+
+    result_image.save(image_filename)
+
+    return calculator.proportion_changed
 
 
 if __name__ == '__main__':
 
-    arguments = get_arguments()
+    parser = argparse.ArgumentParser(description='image resizer')
+    parser.add_argument('imagepath', help='source image path')
+    parser.add_argument('--width', help='result image width', type=int)
+    parser.add_argument('--height', help='result image height', type=int)
+    parser.add_argument('--scale', help='result image scale', type=float)
+    parser.add_argument('--output', help='result image path')
+    args = parser.parse_args()
 
-    if not({'scale', 'width', 'height'} & set(arguments)):
-
+    if not args.scale and not args.width and not args.height:
         print('No one resize option has set')
         print('Run script with [--width, --height | --scale] options')
         sys.exit()
 
-    if ({'scale', 'width'} < set(arguments) or
-            {'scale', 'height'} < set(arguments)):
-
+    if args.scale and args.width or args.scale and args.height:
         print('Options conflict:')
         print('\t--width or --height are not compatible with --scale')
         sys.exit()
 
-    image = Image.open(arguments['image'])
-    calculator = SizeCalculator(image.size)
-    calculator.calculate(arguments)
-
-    if calculator.proportion_changed:
+    if resize_image(
+        args.imagepath,
+        args.scale,
+        args.width,
+        args.height,
+        args.output
+    ):
         print('WARNING: Image proportion has changed')
-
-    result_image = image.resize(calculator.size)
-
-    if {'output'} < set(arguments):
-        result_image.save(arguments['output'])
-        sys.exit()
-
-    filename, file_ext = os.path.splitext(arguments['image'])
-    image_filename = '{}__{}{}'.format(filename, calculator, file_ext)
-    result_image.save(image_filename)
